c7a041b0c03f45f2263fa0c47f91026bcde83308
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-04-08 14:23:35 +0200

    Construct listing of scaled images for folderish content

--- src/folia.sitecontent/folia/sitecontent/gallery.py
+++ src/folia.sitecontent/folia/sitecontent/gallery.py
@@ -1,4 +1,8 @@
+import math
 from five import grok
+
+from Acquisition import aq_inner
+from zope.compoment import getMultiAdapter
 
 from Products.CMFCore.interfaces import IFolderish
 
@@ -8,8 +12,105 @@ class GalleryView(grok.View):
     grok.require('zope2.View')
     grok.name('gallery-view')
 
+    def update(self):
+        self.has_images = len(self.contained_images()) > 0
+
+    def image_list(self):
+        images = self.contained_images()
+        data = []
+        for item in images:
+            info = {}
+            info['title'] = item.Title
+            thumb = self.getImageTag(item, scalename='thumb')
+            info['thumb_url'] = thumb['url']
+            info['thumb_width'] = thumb['width']
+            info['thumb_height'] = thumb['height']
+            original = self.getImageTag(item, scalename='original')
+            info['original_url'] = original['url']
+            info['original_width'] = original['width']
+            info['original_height'] = original['height']
+            data.append(info)
+        return data
+
+    def contained_images(self):
+        context = aq_inner(self.context)
+        items = context.restrictedTraverse('@@folderListing')(
+            portal_type='Image')
+        return items
+
+    def getImageTag(self, item, scalename):
+        obj = item.getObject()
+        scales = getMultiAdapter((obj, self.request), name='images')
+        if scalename == 'thumb':
+            scale = scales.scale('image', width=128, height=71)
+        else:
+            scale = scales.scale('image', width=535, height=300)
+        item = {}
+        if scale is not None:
+            item['url'] = scale.url
+            item['width'] = scale.width
+            item['height'] = scale.height
+        return item
+
 
 class ThumbnailView(grok.View):
     grok.context(IFolderish)
     grok.require('zope2.View')
     grok.name('thumbnail-view')
+
+    def update(self):
+        self.has_images = len(self.contained_images()) > 0
+
+    def image_matrix(self):
+        items = self.image_list()
+        count = len(items)
+        rowcount = count / 3.0
+        rows = math.ceil(rowcount)
+        matrix = []
+        for i in range(int(rows)):
+            row = []
+            for j in range(3):
+                index = 3 * i + j
+                if index <= int(count - 1):
+                    cell = {}
+                    cell['item'] = items[index]
+                    row.append(cell)
+            matrix.append(row)
+        return matrix
+
+    def image_list(self):
+        images = self.contained_images()
+        data = []
+        for item in images:
+            info = {}
+            info['title'] = item.Title
+            thumb = self.getImageTag(item, scalename='thumb')
+            info['thumb_url'] = thumb['url']
+            info['thumb_width'] = thumb['width']
+            info['thumb_height'] = thumb['height']
+            original = self.getImageTag(item, scalename='original')
+            info['original_url'] = original['url']
+            info['original_width'] = original['width']
+            info['original_height'] = original['height']
+            data.append(info)
+        return data
+
+    def contained_images(self):
+        context = aq_inner(self.context)
+        items = context.restrictedTraverse('@@folderListing')(
+            portal_type='Image')
+        return items
+
+    def getImageTag(self, item, scalename):
+        obj = item.getObject()
+        scales = getMultiAdapter((obj, self.request), name='images')
+        if scalename == 'thumb':
+            scale = scales.scale('image', width=128, height=71)
+        else:
+            scale = scales.scale('image', width=535, height=300)
+        item = {}
+        if scale is not None:
+            item['url'] = scale.url
+            item['width'] = scale.width
+            item['height'] = scale.height
+        return item
