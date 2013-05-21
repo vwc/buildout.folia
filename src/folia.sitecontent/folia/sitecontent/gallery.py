2d1d855cdbc975140eac32e6a8eb131b1d16c76c
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-05-21 14:01:52 +0200

    Fix container and make the whole site no longer scalable

--- src/folia.sitecontent/folia/sitecontent/gallery.py
+++ src/folia.sitecontent/folia/sitecontent/gallery.py
@@ -44,7 +44,7 @@ class GalleryView(grok.View):
         if scalename == 'thumb':
             scale = scales.scale('image', width=128, height=71)
         else:
-            scale = scales.scale('image', width=535, height=300)
+            scale = scales.scale('image', width=620, height=535)
         item = {}
         if scale is not None:
             item['url'] = scale.url
